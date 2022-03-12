3a755efc0afc5a67690bc7159063b8a95105d60e
枫阿雨 <2522235302@qq.com>
2022-03-12 10:47:42 +0800

    update-03-12

--- my-blog/blog-parent/blog-api/src/main/java/com/sth666/blog/controller/UploadController.java
+++ my-blog/blog-parent/blog-api/src/main/java/com/sth666/blog/controller/UploadController.java
@@ -1,7 +1,9 @@
 package com.sth666.blog.controller;
 
+import com.sth666.blog.utils.QiniuUtils;
 import com.sth666.blog.vo.Result;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,13 +22,20 @@ import java.util.UUID;
 @RequestMapping("upload")
 public class UploadController {
 
+    @Autowired
+    private QiniuUtils qiniuUtils;
+
     @PostMapping
     public Result upload(@RequestParam("image") MultipartFile file) {
         //原始文件名称 比如aa.png
         String originalFilename = file.getOriginalFilename();
         //唯一的文件名称
-        String fileName = UUID.randomUUID().toString() + "." + StringUtils.substringAfterLast(originalFilename, ".");
-        //上传文件 不建议上传到服务器上
-        return null;
+        String fileName = UUID.randomUUID().toString() + "." + StringUtils.substringAfterLast(file.getOriginalFilename(), ".");        //上传文件 不建议上传到服务器上 上传到七牛云里 可以吧图片发放到离用户最近的服务器上
+        //可以降低我们自身应用服务器的带宽消耗
+        boolean upload = qiniuUtils.upload(file, fileName);
+        if (upload){
+            return Result.success(QiniuUtils.url + fileName);
+        }
+        return Result.fail(20001, "上传失败");
     }
 }
